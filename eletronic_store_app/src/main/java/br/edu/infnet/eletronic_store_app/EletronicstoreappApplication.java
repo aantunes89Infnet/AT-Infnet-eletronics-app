65d1c88d2fa6b78d35b99c1f597d3e572e85225a
andre_da_c_antunes <andre.da.c.antunes@accenture.com>
2021-12-12 16:18:37 -0300

    first commit

---
+++ eletronic_store_app/src/main/java/br/edu/infnet/eletronic_store_app/EletronicstoreappApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.eletronic_store_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletronicstoreappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EletronicstoreappApplication.class, args);
+	}
+
+}
